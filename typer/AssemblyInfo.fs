be5c93b760139061bea149ac5c52d2dcde174169
Ville M. Vainio <ville.vainio@basware.com>
2016-11-29 19:17:52 +0200

    renaming pt1

--- typer/AssemblyInfo.fs
+++ typer/AssemblyInfo.fs
@@ -1,4 +1,4 @@
-﻿namespace typer.AssemblyInfo
+﻿namespace tlitegen.AssemblyInfo
 
 open System.Reflection
 open System.Runtime.CompilerServices
@@ -7,11 +7,11 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("typer")>]
+[<assembly: AssemblyTitle("tlitegen")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("typer")>]
+[<assembly: AssemblyProduct("tlitegen")>]
 [<assembly: AssemblyCopyright("Copyright ©  2016")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
